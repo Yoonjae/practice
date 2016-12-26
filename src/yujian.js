1147298439ac526f9a5a805bc2ce8b6b110f1f87
yujian <yujian@192.168.18.102>
2016-12-26 20:42:30 +0800

    final commit

--- src/yujian.js
+++ src/yujian.js
@@ -7,19 +7,11 @@ require('../sass/homepage.scss');
 (function () {
     require('../lib/bootstrap/assets/javascripts/bootstrap');
 
-
-    var data = {'data':[{'format':'jpg','height':2256,'igroupId':88,'imageCount':1,'imageId':114,'isWeb':'true','name':'2','size':1138118,
-        'thumbnail':'http://www.zhimengzhe.com/d/file/p/2016-11-03/99be6172b982e1f0a7dc0b953fc44a98.jpg','title':'fwef-001',
-        'type':'group','width':3168},{'format':'jpg','height':2256,'igroupId':88,'imageCount':1,'imageId':114,'isWeb':'true','name':'2','size':1138118,
-        'thumbnail':'http://www.zhimengzhe.com/d/file/p/2016-11-03/99be6172b982e1f0a7dc0b953fc44a98.jpg','title':'fwef-001',
-        'type':'group','width':3168},{'format':'jpg','height':2256,'igroupId':88,'imageCount':1,'imageId':114,'isWeb':'true','name':'2','size':1138118,
-        'thumbnail':'http://www.zhimengzhe.com/d/file/p/2016-11-03/99be6172b982e1f0a7dc0b953fc44a98.jpg','title':'fwef-001',
-        'type':'group','width':3168}]};
-
     var Base = require('./base/base'),
         Urls = require('./base/urls')
         ,$body = $('body');
 
+    var topData = {'out':['糯米','新闻','hao123','地图','视频','贴吧','登录','设置']};
     var yujian = function () {
         this._init();
     };
@@ -27,28 +19,30 @@ require('../sass/homepage.scss');
     yujian.prototype._init = function () {
         this.templatebgimage = require('../template/bgimage.html');
 
+        $(document).ready(function () {
+            $('.searchinput').focus();
+        });
+
+        $('.moreinfo').mouseenter(function () {
+            $('.hideright').show();
+        });
+        $('.hideright').mouseleave(function () {
+            $('.hideright').hide();
+        });
+
+
+        $('.searchinput').focus(function(){
+            $('.bigsearch').css('border-color','#4791ff');
+        });
+        $('.searchinput').blur(function () {
+            $('.bigsearch').css('border-color','lightgray');
+        });
         this.getbgimage();
     };
 
     yujian.prototype.getbgimage = function () {
         var self = this;
-        var postData = {
-            start : 0,
-            length:10,
-            flag:1
-        };
 
-        $('#bgimage').html(self.templatebgimage({list:data.data}));
-
-        // Base.get(Urls.imageList.getJsonByType,postData,function (res) {
-        //     console.log(res);
-        // });
-
-        $('.imgsource').on('click',function () {
-           Base.confirm('删除','确认删除',function ($model) {
-               $model.modal('hide');
-           }) ;
-        });
     };
 
     new yujian();
